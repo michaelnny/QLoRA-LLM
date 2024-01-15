9cbab7bc56069f02ad205deeadce1fd862522923
Michael Hu <michaelnny@gmail.com>
2024-01-15 10:46:52 +0800

    fix bug in trackers

--- qlora_llm/utils/tracker.py
+++ qlora_llm/utils/tracker.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2023 Michael Hu.
+# This project is released under the MIT License.
+# See the accompanying LICENSE file for details.
+
+
 from typing import Dict
 import numpy as np
 
@@ -9,10 +14,10 @@ class StatsTracker:
     """Tracker for LLM model during fine-tune"""
 
     def __init__(self, distributed: bool = False, rank=0):
-        self.metrics = torch.zeros(5).to(rank if distributed else 'cpu')
-        self.c = 0
-
         self.distributed = distributed
+        self.rank = rank
+
+        self.reset()
 
     def update(self, loss: torch.Tensor, num_accurate: int, num_samples: int):
         metrics = self.metrics
@@ -26,7 +31,7 @@ class StatsTracker:
         self.c += 1
 
     def reset(self) -> None:
-        self.metrics = torch.zeros(5)
+        self.metrics = torch.zeros(5).to(f'cuda:{self.rank}' if self.distributed else 'cuda')
         self.c = 0
 
     def get_dict(self) -> Dict:
@@ -43,51 +48,3 @@ class StatsTracker:
         accuracy = 100 * metrics[3] / metrics[4]
 
         return {'loss': loss.item(), 'accuracy': accuracy.item(), 'perplexity': perplexity.item()}
-
-
-class RMStatsTracker:
-    """Tracker for reward model"""
-
-    def __init__(self, distributed: bool = False, rank=0):
-        self.metrics = torch.zeros(6).to(rank if distributed else 'cpu')
-        self.c = 0
-
-        self.distributed = distributed
-
-    def update(self, loss: torch.Tensor, num_accurate: int, num_samples: int, reward_best: float, reward_worst: float):
-        metrics = self.metrics
-
-        metrics[0] += loss.item()  # sum up batch loss
-        metrics[1] += 1  # increase number of samples
-        metrics[2] += num_accurate  # sum up number of accurate prediction tokens
-        metrics[3] += num_samples  # sum up number of responses or combinations
-        metrics[4] += reward_best  # sum up best reward
-        metrics[5] += reward_worst  # sum up worst reward
-
-        self.c += 1
-
-    def reset(self) -> None:
-        self.metrics = torch.zeros(6)
-        self.c = 0
-
-    def get_dict(self) -> Dict:
-        if self.c == 0:
-            return {}
-
-        metrics = self.metrics
-
-        if self.distributed:
-            dist.all_reduce(metrics, op=dist.ReduceOp.SUM)
-
-        loss_mean = metrics[0] / metrics[1]
-        accuracy = 100 * metrics[2] / metrics[3]
-        preferred_reward_mean = metrics[4] / metrics[1]
-        rejected_reward_mean = metrics[5] / metrics[1]
-
-        return {
-            'loss': loss_mean.item(),
-            'accuracy': accuracy.item(),
-            'preferred_reward': preferred_reward_mean.item(),
-            'rejected_reward': rejected_reward_mean.item(),
-            'reward_gap': (preferred_reward_mean - rejected_reward_mean).item(),
-        }
